a0ce6ec1d416584b5ef1d93d76f66148e6b1884b
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-10-31 14:27:04 +0000

    Add styling to game

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import './App.css';
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <div className="screen">
-        <div className="control">
-          <span className="icon">&#129307;</span>
-          <button>Rock</button>
-        </div>
-        <div className="control">
-          <span className="icon">&#x1F91A;</span>
-          <button>Paper</button>
-        </div>
-        <div className="control">
-          <span className="icon">&#9996;</span>
-          <button>Scissors</button>
+    <>
+      <div className="App">
+        <div className="screen">
+          <div className="control">
+            <span className="icon">&#129307;</span>
+            <button className="button">Rock</button>
+          </div>
+          <div className="control">
+            <span className="icon">&#x1F91A;</span>
+            <button className="button">Paper</button>
+          </div>
+          <div className="control">
+            <span className="icon">&#9996;</span>
+            <button className="button">Scissors</button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
