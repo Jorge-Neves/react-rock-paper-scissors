a025d3a97a32b6f058ca2e241cf3e3be24d6186c
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-10-31 14:20:20 +0000

    Add layout for controls

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import './App.css';
 const App: FC = () => {
   return (
     <div className="App">
-      <div>
-        <div>
-          <span>&#129307;</span>
+      <div className="screen">
+        <div className="control">
+          <span className="icon">&#129307;</span>
           <button>Rock</button>
         </div>
-        <div>
-          <span>&#x1F91A;</span>
+        <div className="control">
+          <span className="icon">&#x1F91A;</span>
           <button>Paper</button>
         </div>
-        <div>
-          <span>&#9996;</span>
+        <div className="control">
+          <span className="icon">&#9996;</span>
           <button>Scissors</button>
         </div>
       </div>
